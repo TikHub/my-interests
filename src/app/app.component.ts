de0676b38f51f801c392d4e6e3aae03b16ccbafc
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2022-11-29 00:39:14 +0400

    [feature] - Added Header and Footer components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, HeaderComponent, FooterComponent],
 })
 export class AppComponent {
   title = 'my-interests';
